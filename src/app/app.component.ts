1490293df3f06241509498898501eb74c683a53e
chaitanya_sekeltech <chaitanya.kulkarni@sekel.tech>
2023-01-21 22:40:36 +0530

    Initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../services/api.service'
+import { Post } from '../interfaces/posts'
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent implements OnInit {
+  title = 'test-app';
+  posts: any[] = []
+  isLoading: boolean = false
+  option: string = ''
+
+  constructor (private apiService: ApiService) {}
+
+  ngOnInit(): void {
+    
+  }
+
+  fetchData (option: string) {
+    this.isLoading = true
+    this.apiService.getPosts(option).subscribe(data => {
+      this.posts = data
+      this.isLoading = false
+    })
+  }
+}
---
+++ src/services/api.service.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { Post } from '../interfaces/posts'
+
+@Injectable({
+    providedIn: 'root'
+})
+export class ApiService {
+    // option: string = 'users'
+    
+    constructor (private http: HttpClient) {}
+    
+    getPosts (option: string): Observable<any> {
+        const API_URL = `https://jsonplaceholder.typicode.com/${option}`
+        return this.http.get<any>(API_URL)
+    }
+}
